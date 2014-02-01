cf6d9104477e70e4fcd7f8ec52e65e309cbd363e
Katy Campen <katycampen@gmail.com>
2014-02-01 17:21:49 -0500

    compose location update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,2 @@
 
 
-
-
-$('.share').click(function(){
-	$(this).css('height', '200px', 'width', '625px');
-});
-
-
-
-
-$('.sharebutton').bind("mouseenter mouseleave",function(){
-	$(this).toggleClass('buttonshadow');
-});
