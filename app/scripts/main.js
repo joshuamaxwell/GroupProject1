742ec18f93b68e330b93f0a3ace1aa2926566845
Katy Campen <katycampen@gmail.com>
2014-02-01 15:38:17 -0500

    updated compose section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+
+
+
+
+$('.share').click(function(){
+	$(this).css('height', '200px', 'width', '625px');
+});
+
+
+
+
+$('.sharebutton').bind("mouseenter mouseleave",function(){
+	$(this).toggleClass('buttonshadow');
+});
