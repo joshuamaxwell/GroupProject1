ded9c328d11c4c212e60ff40bb8061dd1d365a8b
JMaxwell <maxwell.joshua@gmail.com>
2014-02-01 20:22:43 -0500

    tweet prepending

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,25 @@
+var userProfile = {
+  name: "Billy Bob Joe",
+  location: "Table Top Rock",
+  avatar: "http://www.lorempixel.com/150/150/people/3"
+};
+
+var Tweet = function (userProfile) {
+  this.name = userProfile.name;
+  this.location = $('.sharelocation').val();
+  this.msg = $('.share').val();
+  this.avatar = userProfile.avatar;
+  this.time = 'Feb 1, 2014 6:30pm';
+  this.isFavorite = false;
+};
+
+tweetArray = [];
+
+var postTemplate = _.template($('.tweet-template').text());
 
 
+$('.sharebutton').click( function() {
+  var tweet = new Tweet(userProfile);
+  tweetArray.push(tweet);
+  $('.tweets').prepend( postTemplate(tweet) );
+});
